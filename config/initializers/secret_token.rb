0f63f3b1179ba8254436ff6d42b503ab2d96b255
Johan Andersson von Geijer <johananderssonvongeijer@gmail.com>
2012-01-08 22:36:49 +0100

    Added a complete structure for a rails web application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeReSuTo::Application.config.secret_token = '6f6d01dd9915d80667d0728e761ba081dc22d54e4f7cb7814f43d7f7e938da54866175c68a2820d2c145f6dd7d0452e7ed3d3f861e7e80bd5abc423973d0e932'
